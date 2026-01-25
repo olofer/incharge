460512d775cbf6e60952876c1523e6d3e09ccdbf
olofer <k.e.j.olofsson@gmail.com>
2026-01-25 08:35:14 -0800

    Optimize and extend basic sinusoidal example (E-field)

--- basic-version/retarded.js
+++ basic-version/retarded.js
@@ -37,6 +37,7 @@ const fragmentShaderSource = `#version 300 es
   uniform vec2 u_rho;
   uniform float u_time;
   uniform float u_zoom;
+  uniform float u_contrast;
   uniform float u_beta;
   uniform float u_freq;
   uniform int u_style;
@@ -50,19 +51,27 @@ const fragmentShaderSource = `#version 300 es
     return vec2(cs * osc.x - sn * osc.y, sn * osc.x + cs * osc.y);
   }
 
-  vec4 pos_vel(vec2 osc, vec2 rho, float omega) {
+  /*vec4 pos_vel(vec2 osc, vec2 rho, float omega) {
     float cs = osc.x;
     float sn = osc.y;
     return vec4(cs * rho.x, sn * rho.y, -1.0 * omega * rho.x * sn, omega * rho.y * cs);
+  }*/
+
+  vec2[3] pos_vel_acc(vec2 osc, vec2 rho, float omega) {
+    float cs = osc.x;
+    float sn = osc.y;
+    vec2[3] rva;
+    rva[0] = vec2(cs * rho.x, sn * rho.y);
+    rva[1] = omega * vec2(-1.0 * rho.x * sn, rho.y * cs);
+    rva[2] = -1.0 * omega * omega * vec2(rho.x * cs, rho.y * sn);
+    return rva;
   }
 
   // Retarded time tau > 0 is found with bisecting the root of this function
   float root_function(vec2 osc, vec2 rho, float omega, float c, vec2 pos, float tau) {
     vec2 osc_ = backroll_osc(osc, omega, tau);
     vec2 src = osc_ * rho;
-    float dx = pos.x - src.x;
-    float dy = pos.y - src.y;
-    return sqrt(dx * dx + dy * dy) - c * tau;
+    return length(pos - src) - c * tau;
   }
 
   // Bisect the retarded time tau for the present oscillator state and the present location 
@@ -89,6 +98,15 @@ const fragmentShaderSource = `#version 300 es
     return (tau0 + tau1) / 2.0;
   }
 
+  vec2 electric_field(float s, float BETA, float c, vec2 R, vec2 beta, vec2 vdot) {
+    float s2 = s * s;
+    float a1 = (1.0 - BETA * BETA) / (s2 * s);
+    float c2s2 = c * c * s2;
+    float a2 = dot(vdot, R) / (c2s2 * s);
+    float Rabs = length(R);
+    return a1 * R - a1 * Rabs * beta + a2 * R - a2 * Rabs * beta - Rabs * vdot / c2s2;
+  }
+
   void main() {
     // TODO: color squash, compression/contrast transformations (c)
 
@@ -108,35 +126,33 @@ const fragmentShaderSource = `#version 300 es
     float tau = bisect_tau(osc, rho, OMEGA, c, uv);
 
     vec2 osc_tau = backroll_osc(osc, OMEGA, tau);
-    vec4 src = pos_vel(osc_tau, rho, OMEGA);
-    vec2 rsrc = uv - src.xy;
-    float betax = src.z / c;
-    float betay = src.w / c;
+    vec2[3] rva = pos_vel_acc(osc_tau, rho, OMEGA);
+    vec2 rsrc = uv - rva[0];
+    vec2 beta = rva[1] / c;
 
     float Rtau = c * tau;
-    float Rmod = Rtau - betax * rsrc.x - betay * rsrc.y;
-
-    // Encode the potential (phi, Ax, Ay) in RGB "somehow"
+    float Rmod = Rtau - dot(beta, rsrc);
 
     float Q = 1.0 / (1.0 + Rmod);
-    float Ax = betax / (1.0 + Rmod);
-    float Ay = betay / (1.0 + Rmod);
+    vec2 A = beta / (1.0 + Rmod);
 
     float alpha = 1.0;
     vec3 color;
 
     if (u_style == 0) {
-      color = vec3(0.0, sqrt(Ax * Ax + Ay * Ay), Q);
+      color = vec3(0.0, dot(A, A), Q);
     } else if (u_style == 1) {
-      color = vec3(0.0, Rtau / u_zoom , Q);
+      vec2 E = electric_field(1.0 + Rmod, BETA, c, rsrc, beta, rva[2]);
+      color = vec3(0.0, dot(E, E) , 0.0);
     } else if (u_style == 2) {
       color = vec3(Rmod / u_zoom, 0.0, 0.0);
     } else if (u_style == 3) {
       color = vec3(Q, 0.0, 0.0);
     } else {
-      color = vec3(abs(Ax), 0.0, abs(Ay));
+      color = vec3(abs(A.x), 0.0, abs(A.y));
     }
 
+    if (u_contrast != 1.0) color = pow(color, vec3(u_contrast));
     fragColor = vec4(color, alpha);
   }
 `;
@@ -179,6 +195,7 @@ const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution')
 const rhoUniformLocation = gl.getUniformLocation(program, 'u_rho');
 const timeUniformLocation = gl.getUniformLocation(program, 'u_time');
 const zoomUniformLocation = gl.getUniformLocation(program, 'u_zoom');
+const contrastUniformLocation = gl.getUniformLocation(program, 'u_contrast');
 const betaUniformLocation = gl.getUniformLocation(program, 'u_beta');
 const freqUniformLocation = gl.getUniformLocation(program, 'u_freq');
 const styleUniformLocation = gl.getUniformLocation(program, 'u_style');
@@ -213,6 +230,7 @@ let filteredFPS = 0.0;
 let lastTime = performance.now();
 let simTime = 0.0;
 let zoomLevel = 8.0;
+let contrastLevel = 1.0;
 let betaLevel = 0.85;
 let rho_xy = [1.0, 0.5];
 let freqValue = 0.25;
@@ -221,7 +239,7 @@ function keyDownEvent(e) {
   let code = e.keyCode;
   let key = e.key;
 
-  if (key =='Tab') {
+  if (key == 'Tab') {
     plotStyle += e.shiftKey ? -1 : 1;
     if (plotStyle < 0) plotStyle = numPlotStyles - 1;
     if (plotStyle == numPlotStyles) plotStyle = 0;
@@ -289,6 +307,11 @@ function keyDownEvent(e) {
     return;
   }
 
+  if (key == 'c' || key == 'C') {
+    contrastLevel *= (e.shiftKey ? 1.25 : 0.80);
+    return;
+  }
+
 }
 
 window.addEventListener('keydown', keyDownEvent);
@@ -302,23 +325,21 @@ function render() {
   if (elapsedTimeSeconds > 0.0 && elapsedTimeSeconds < 1.0)
     filteredFPS = (betaFPSfilter) * (1.0 / elapsedTimeSeconds) + (1.0 - betaFPSfilter) * filteredFPS;
 
-  // Update uniforms
   gl.useProgram(program);
   gl.uniform2f(resolutionUniformLocation, canvasgl.width, canvasgl.height);
   gl.uniform2f(rhoUniformLocation, rho_xy[0], rho_xy[1]);
   gl.uniform1f(timeUniformLocation, simTime);
   gl.uniform1f(zoomUniformLocation, zoomLevel);
+  gl.uniform1f(contrastUniformLocation, contrastLevel);
   gl.uniform1f(betaUniformLocation, betaLevel);
   gl.uniform1f(freqUniformLocation, freqValue);
   gl.uniform1i(styleUniformLocation, plotStyle);
 
-  // Clear and draw
   gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.bindVertexArray(vao);
   gl.drawArrays(gl.TRIANGLES, 0, 6);
 
-  // Text overlay
   const ctx = canvas2d.getContext('2d');
   ctx.clearRect(0, 0, canvas2d.width, canvas2d.height);
   ctx.globalAlpha = 1.0;
@@ -327,7 +348,6 @@ function render() {
   ctx.fillText('<fps> = ' + filteredFPS.toFixed(1), 20.0, canvas2d.height - 25.0);
   ctx.fillText('[b] beta = ' + betaLevel.toFixed(4) + ' [f] (anim.) freq = ' + freqValue.toFixed(4), 20.0, 25.0);
 
-  // Request next frame
   requestAnimationFrame(render);
 }
 
